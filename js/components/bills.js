a21ba50a08e5d7380cb7472d1c24091a855663bc
Jason Hamm <jasonhamm.me@gmail.com>
2015-09-03 15:32:39 -0600

    Add a list of bills

--- js/components/bills.js
+++ js/components/bills.js
@@ -3,9 +3,15 @@ import React from 'react';
 export default class Bills extends React.Component {
 	render() {
 		return (
-			<div>
+			<div className="bills">
 				<h1>Bills</h1>
-				
+				<ul>
+					<li>Bill 1</li>
+					<li>Bill 2</li>
+					<li>Bill 3</li>
+					<li>Bill 4</li>
+					<li>Bill 5</li>
+				</ul>
 			</div>
 		)
 	}
