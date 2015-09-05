303e77ca59e7cf943bd0fc4c615c98b62cc1ede4
Jason Hamm <jasonhamm.me@gmail.com>
2015-09-05 14:46:51 -0600

    Fix untracked files

--- js/router.js
+++ js/router.js
@@ -3,10 +3,13 @@ require('./../sass/app.scss');
 import React from 'react';
 import { Router, Route, Link } from 'react-router';
 import { history } from 'react-router/lib/HashHistory';
+import fetch from 'isomorphic-fetch';
 
 var Main = require('./components/main.js');
 var Bills = require('./components/bills.js');
 var About = require('./components/about.js');
+
+fetch('http://swapi.co/api/people/1')
 
 React.render((
   <Router history={history}>
