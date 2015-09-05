02566fa1db7863a226b2496de61046d755dc9194
Jason Hamm <jasonhamm.me@gmail.com>
2015-09-05 15:31:04 -0600

    Add util to make rest calls

---
+++ js/utils/fetch.utils.js
@@ -0,0 +1,8 @@
+export function isoFetch(url) {
+	return fetch(url).then(function(response) {
+			if (response.status >= 400) {
+				throw new Error("Bad response from server");
+			}
+			return response.json();
+		})
+}
