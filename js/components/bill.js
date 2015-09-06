59f544234ead37e125f24f0a6552fd2342054873
Jason Hamm <jasonhamm.me@gmail.com>
2015-09-06 00:47:27 -0600

    Format currency and dates

--- js/components/bill.js
+++ js/components/bill.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import numeral from 'numeral';
+import moment from 'moment';
 
 var Bill = React.createClass({
 	getInitialState() {
@@ -9,7 +11,21 @@ var Bill = React.createClass({
 
 	getExpandedDOM(bill) {
 		if(this.state.expanded) {
-			return <div>{bill.name}</div>
+			var payDates = bill.pay_dates.map(date => {
+				return (
+					<li>
+						<div className="flex-container">
+							<div className="bill-width">{moment(date.pay_date).format('DD MMM YYYY')}</div>
+							<div className="bill-width">{numeral(date.payment).format('$0,0.00')}</div>
+						</div>
+					</li>
+				)
+			});
+			return (
+				<ul>
+					{payDates}
+				</ul>
+			)			
 		} else {
 			return null;
 		}
@@ -22,7 +38,7 @@ var Bill = React.createClass({
 				<div className="flex-container-col" onClick={this.expand}>
 					<div className="flex-container">
 						<div className="bill-width">{this.props.bill.name}</div>
-						<div className="bill-width">{this.props.bill.payoff}</div>
+						<div className="bill-width">{numeral(this.props.bill.payoff).format('$0,0.00')}</div>
 					</div>
 					{ExpandedBill}
 				</div>
