c058bdf0bb3c26b033fd17c0bc22b6d0b7b51056
Jason Hamm <jasonhamm.me@gmail.com>
2015-09-05 20:54:51 -0600

    Expand row on click

--- js/components/bill.js
+++ js/components/bill.js
@@ -1,14 +1,38 @@
 import React from 'react';
 
-export default class Bill extends React.Component {
+var Bill = React.createClass({
+	getInitialState() {
+		return {
+			expanded : false
+		}
+	},
+
+	getExpandedDOM(bill) {
+		if(this.state.expanded) {
+			return <h1>Jason</h1>
+		} else {
+			return null;
+		}
+	},
+
 	render() {
+		var ExpandedBill = this.getExpandedDOM(this.props.bill);
 		return (
 			<li>
-				<div className="flex-container">
-					<div>{this.props.bill.name}</div>
-					<div>{this.props.bill.payoff}</div>
+				<div className="flex-container" onClick={this.expand}>
+					<div className="bill-width">{this.props.bill.name}</div>
+					<div className="bill-width">{this.props.bill.payoff}</div>
+					{ExpandedBill}
 				</div>
 			</li>
 		)
+	},
+
+	expand() {
+		this.setState({
+			expanded : !this.state.expanded
+		});
 	}
-}
+})
+
+module.exports = Bill;
