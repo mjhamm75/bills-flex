47e40bff0588c82b8357846eef6804476a12f29f
Jason Hamm <jasonhamm.me@gmail.com>
2015-09-07 09:03:30 -0600

    Convert to old style react class and add sorting

--- js/components/bills.js
+++ js/components/bills.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import Bill from './bill.js';
 
-export default class Bills extends React.Component {
-	billDOM(bills) {
-		return this.props.billsList.map(bill => {
+module.exports =  React.createClass({
+	billDOM: function(bills) {
+		var orderBy = this.state.orderBy;
+		var ordered = this.props.billsList.sort((a, b) => {
+			if(orderBy === "payoff") {
+				return parseFloat(a[orderBy]) > parseFloat(b[orderBy]);				
+			} else {
+				return a[orderBy] > b[orderBy];
+			}
+		})
+		return ordered.map(bill => {
 			return <Bill key={bill.id} bill={bill} />
 		})
-	}
-	render() {
+	},
+
+	getInitialState: function() {
+		return {
+			orderBy: 'name'
+		}	
+	},
+
+	render: function() {
 		var bills = this.billDOM(this.props.bills);
 		return (
 			<div className="table">
 				<div className="row">
-					<div className="cell">Bill Name</div>
-					<div className="cell">Amount</div>
+					<div className="cell" onClick={this.sort.bind(this, "name")}>Bill Name</div>
+					<div className="cell" onClick={this.sort.bind(this, "payoff")}>Amount</div>
 				</div>
 				{bills}
 			</div>
 		)
+	},
+	sort: function(orderBy) {
+		this.setState({
+			orderBy: orderBy
+		})
 	}
-}
+});
