f2cab6bce4d84bf246b13df168d360576550e416
Jason Hamm <jasonhamm.me@gmail.com>
2015-09-09 07:59:54 -0600

    Add sample sql

---
+++ sql/bills.sql
@@ -0,0 +1,3 @@
+insert into bills(name, payoff, type) values('Water', 0, null);
+
+insert into bills_paid_dates(id, bill_id, payment, due_date, pay_date, paid) values(7, 5, 45.00, '2015-09-09', '2015-09-09', true);
