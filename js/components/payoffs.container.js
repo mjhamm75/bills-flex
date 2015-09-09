1c48859f5dfd5df0529282f2402ed9f1dfc8dffe
Jason Hamm <jasonhamm.me@gmail.com>
2015-09-08 21:30:24 -0600

    Create container/route for payoffs

---
+++ js/components/payoffs.container.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Bills from './bills.js';
+import BillStore from './../stores/bill.store.js';
+var BillActions = require('./../actions/bill.actions.js');
+
+function getBills() {
+	return BillStore.getBills();
+}
+
+var PayoffsContainer = React.createClass({
+	componentWillMount: function() {
+		BillStore.addChangeListener(this._onChange);
+	},
+
+	getInitialState: function() {
+		BillActions.updateBills();
+		return {
+			bills: getBills()
+		}
+	},
+
+	_onChange: function() {
+		this.setState({
+			bills: getBills()
+		});
+	},
+
+	render: function() {
+		return (
+			<div className="bills">
+				<h1>Payoff</h1>
+				<Bills billsList={this.state.bills} />
+			</div>
+		)
+	}
+});
+
+module.exports = PayoffsContainer;
--- js/router.js
+++ js/router.js
@@ -7,12 +7,14 @@ import fetch from 'isomorphic-fetch';
 
 import Main from './components/main.js';
 var BillsContainer = require('./components/bills.container.js');
+var PayoffsContainer = require('./components/payoffs.container.js');
 import About from './components/about.js';
 
 React.render((
   <Router history={history}>
     <Route path="/" component={Main}>
       <Route path="bills" component={BillsContainer}/>
+      <Route path="payoff" component={PayoffsContainer}/>
       <Route path="about" component={About}/>
     </Route>
   </Router>
